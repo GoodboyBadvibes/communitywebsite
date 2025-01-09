9423de92c1ddda14f772a86446ba5dde0d2ed246
GoodboyBadvibes <eekwebene@gmail.com>
2025-01-09 15:08:12 +0100

    created page.js

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../app/UI/components/Nav'
+
+const page = () => {
+  return (
+    <>
+      <div>
+        <Nav />
+      </div>
+    </>
+  )
+}
+
+export default page
