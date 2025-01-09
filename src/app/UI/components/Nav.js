92372c65a1f85628e1314d25c5d6103de63e28c2
GoodboyBadvibes <eekwebene@gmail.com>
2025-01-09 15:10:34 +0100

    created nav component and started initial styling

---
+++ src/app/UI/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+      <div>
+        <h1 className={`header1 text-standard` }>Ogidi</h1>
+      </div>
+    </>
+  )
+}
+
+export default Nav
